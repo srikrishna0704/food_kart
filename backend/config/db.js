642937341054eb2ce026f803e33ed7123d2ba168
srikrishna0704 <srikrishna0704@gmail.com>
2026-02-14 20:06:20 +0530

    fixed mongodb env variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
