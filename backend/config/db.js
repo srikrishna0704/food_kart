52d72e71bc62eaab28b81a633685b8f0f5a41855
srikrishna0704 <srikrishna0704@gmail.com>
2026-02-14 21:00:14 +0530

    fixed mongoose connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
